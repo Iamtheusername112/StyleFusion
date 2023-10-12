d7f9d44e1213153c09e672928a8d809181b3016a
Iamtheusername112 <apkfem@gmail.com>
2023-10-12 11:11:04 +0200

    setting and configuring the database

--- api/app.js
+++ api/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
-require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./db");
 
 const app = express();
+
+require("dotenv").config();
+
+// Connect to the database
+connectDB();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.json("Hello world");
+});
 
 //Running the server
 const server = process.env.PORT;
