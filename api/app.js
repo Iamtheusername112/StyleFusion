541f5d2f93d7838ef12ad995ad28fb3da492e58b
Iamtheusername112 <apkfem@gmail.com>
2023-10-19 09:45:50 +0200

    update

--- api/app.js
+++ api/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const morgan = require("morgan");
 const connectDB = require("./db");
 
